f8af6181c2685ba3e92a9648b9505a57d46e267e
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-06 16:46:21 -0500

    Apply

---
+++ Apply/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_version = "~> 1.3"
+  cloud {
+    organization = "kusdeswa"
+
+    workspaces {
+      name = "kusdeswa"
+ 
+    }
+   }
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 6.2.0"      }
+        random = {
+            source = "hashicorp/random"
+            version = "~> 3.1.0"
+        }
+    }
+}
