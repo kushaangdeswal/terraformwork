a62cb231af05ef57f5e941e733b94a816f2e439b
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-06 11:04:12 -0500

    data block

---
+++ DataBlock/main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+    region = "us-east-1"
+}
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners      = ["099720109477"] # Canonical
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+}
+resource "aws_instance" "app_server" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = t2.micro
+    tags = {
+        Name = "AppServerInstance"
+    }
+}
