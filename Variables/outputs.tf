87de872e9158d8dffcf069c6a4f70c22e004be83
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-06 13:29:47 -0500

    variables

--- Variables/outputs.tf
+++ Variables/outputs.tf
@@ -2,3 +2,7 @@ output "hostname" {
     description = "The public DNS name of the EC2 instance"
     value       = aws_instance.app_server.public_dns
 }
+output "subnet_id" {
+    description = "The subnet ID where the instance is deployed"
+    value       = aws_instance.app_server.subnet_id
+}
