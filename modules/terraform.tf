4429d31cbf0caad614336d672f20a5327e326182
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-06 15:13:37 -0500

    modules

--- modules/terraform.tf
+++ modules/terraform.tf
@@ -1,5 +1,13 @@
 terraform {
     required_version = "~> 1.3"
+  cloud {
+    organization = "kusdeswa"
+
+    workspaces {
+      name = "kusdeswa"
+ 
+    }
+   }
     required_providers {
         aws = {
             source  = "hashicorp/aws"
