1670084219c0e8ed9105c13abb81a2b93d92ed92
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-06 13:17:22 -0500

    variables

--- Variables/variables.tf
+++ Variables/variables.tf
@@ -1,10 +1,15 @@
-variable "instance_name" {
-    description = "Name of the instance"
+variable "region" {
+    description = "AWS region"
     type        = string
-    default     = "AppServerInstance"
+    default     = "us-west-2"
 }
 variable "instance_type" {
     description = "Type of the instance"
     type        = string
     default     = "t2.micro"
+}
+variable "instance_name" {
+    description = "Name of the instance"
+    type        = string
+    default     = "AppServerInstance"
 }
