49400ec9c8ac9da969e82e856c9f0cf6296fdb78
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-06 11:34:50 -0500

    data block

--- DataBlock/main.tf
+++ DataBlock/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+    region = "us-west-2"
 }
 data "aws_ami" "ubuntu" {
     most_recent = true
@@ -12,7 +12,7 @@ data "aws_ami" "ubuntu" {
 }
 resource "aws_instance" "app_server" {
     ami = data.aws_ami.ubuntu.id
-    instance_type = t2.micro
+    instance_type = "t2.micro"
     tags = {
         Name = "AppServerInstance"
     }
