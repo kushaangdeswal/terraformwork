ee17aa1c0d205071064d104cf774cf627de272b0
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-12 14:04:29 -0500

    Dependencies

---
+++ DependsOn/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region = "us-west-2"
+
+}
+data "aws_ami" "ubuntu" {
+    most_recent = true
+    owners      = ["099720109477"] # Canonical
+
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+}
+resource "aws_instance" "example" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+}
+
+resource "aws_instance" "example_2" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+}
+resource "aws_eip" "ip" {
+   instance = aws_instance.example.id
+}
