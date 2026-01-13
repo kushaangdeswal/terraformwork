96a2b5618e60db49fb6796089026c64ef55b2486
kushaangdeswal <kushaangdeswal@outlook.com>
2026-01-12 19:56:55 -0500

    ConditionalLoop

---
+++ ConditionLoop/terraform.tf
@@ -0,0 +1,19 @@
+terraform { 
+    required_version = ">=1.2.0"
+    cloud { 
+        organization = "kusdeswa"
+        workspaces {
+            name = "kusdeswa"
+        }   
+    }
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.0"  
+        }
+        random = {
+            source  = "hashicorp/random"
+            version = "~> 3.0"
+        }
+    }
+}
